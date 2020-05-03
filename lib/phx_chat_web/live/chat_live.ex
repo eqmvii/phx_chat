005277a87de81408343301d9b670fe160a913553
Eric (Martin) Mancini <coelomate@gmail.com>
2020-05-03 19:03:16 -0400

    pick a username

--- lib/phx_chat_web/live/chat_live.ex
+++ lib/phx_chat_web/live/chat_live.ex
@@ -8,21 +8,30 @@ defmodule PhxChatWeb.ChatLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    PubSub.subscribe(PhxChat.PubSub, "chat_messages")
-
-    {:ok, assign(socket, message: "", message_list: recent_messages())}
+    {:ok, assign(socket, message: "", message_list: [], username: nil)}
   end
 
   @impl true
-  def handle_event("send_chat", %{"message_input" => message} = wut, socket) do
-    Chat.create_message(%{user: inspect(self()), message: message})
+  def handle_event("send_chat", %{"message_input" => message}, %{assigns: %{username: username}} = socket) do
+    Chat.create_message(%{user: username, message: message})
 
     PubSub.broadcast_from(PhxChat.PubSub, self(), "chat_messages", "new_message")
 
     {:noreply,
       socket
-      |> put_flash(:info, "Message Sent: \"#{message}\"")
       |> assign(message: "")
+      |> assign(message_list: recent_messages())
+    }
+  end
+
+  @impl true
+  def handle_event("login", %{"username_input" => username}, socket) do
+    PubSub.subscribe(PhxChat.PubSub, "chat_messages")
+
+    {:noreply,
+      socket
+      |> put_flash(:info, "Welcome to the Chat, #{username}!")
+      |> assign(username: username)
       |> assign(message_list: recent_messages())
     }
   end
@@ -31,6 +40,7 @@ defmodule PhxChatWeb.ChatLive do
   # PubSub Subscribtion Handler #
   ###############################
 
+  @impl true
   def handle_info(message, socket) do
     IO.puts "\n[][][][] New Message Received by #{inspect(self())}[][][][]"
     {:noreply, assign(socket, message_list: recent_messages)}
