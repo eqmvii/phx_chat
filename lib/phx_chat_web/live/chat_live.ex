13b85d52436af7068e810d01535bb158ef8c09e8
Eric (Martin) Mancini <coelomate@gmail.com>
2020-05-03 13:48:36 -0400

    chat prototype pre-pubsub

--- lib/phx_chat_web/live/chat_live.ex
+++ lib/phx_chat_web/live/chat_live.ex
@@ -1,19 +1,23 @@
 defmodule PhxChatWeb.ChatLive do
   use PhxChatWeb, :live_view
 
+  alias PhxChat.Chat
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, msg: "", results: %{})}
+    {:ok, assign(socket, message: "", message_list: Chat.list_messages())}
   end
 
   @impl true
-  def handle_event("send_chat", %{"msg_input" => msg_input}, socket) do
-    IO.inspect msg_input
+  def handle_event("send_chat", %{"message_input" => message}, socket) do
+    # TODO ERIC move to service / context?
+    Chat.create_message(%{user: "current_user", message: message})
 
     {:noreply,
       socket
-      |> put_flash(:info, "Message Sent: \"#{msg_input}\"")
+      |> put_flash(:info, "Message Sent: \"#{message}\"")
       |> assign(msg: "")
+      |> assign(message_list: Chat.list_messages())
     }
   end
 end
