23cb3b5408c4dbecc919910a59e7823ea91d72ee
Eric (Martin) Mancini <coelomate@gmail.com>
2020-05-07 22:00:11 -0400

    auth plug tests

---
+++ test/phx_chat_web/plugs/auth_plug_test.exs
@@ -0,0 +1,30 @@
+defmodule PhxChatWeb.AuthPlugTest do
+  use PhxChatWeb.ConnCase
+
+  alias PhxChatWeb.AuthPlug
+
+  test "returns an unaltered conn for authed users", %{conn: conn} do
+    authed_conn = init_test_session(conn, %{username: "Tester"})
+
+    post_plug_conn = AuthPlug.call(authed_conn, %{})
+
+    assert is_nil(post_plug_conn.status)
+
+    assert_raise RuntimeError, fn ->
+      redirected_to(post_plug_conn)
+    end
+
+    assert authed_conn == post_plug_conn
+  end
+
+  test "redirects if not logged in", %{conn: conn} do
+    unauthed_conn =
+      conn
+      # init_test_session is Phoenix magic that fakes a session, which the auth plug requires
+      |> init_test_session(%{})
+      |> AuthPlug.call(%{})
+
+    assert unauthed_conn.status == 302
+    assert redirected_to(unauthed_conn) == "/login"
+  end
+end
