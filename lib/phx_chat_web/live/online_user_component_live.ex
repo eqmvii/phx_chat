6f8fe5e9cd39c0ae17d8ba4a39423e224aef61f3
Eric (Martin) Mancini <coelomate@gmail.com>
2020-05-23 21:58:43 -0400

    flexbox basic layout

--- lib/phx_chat_web/live/online_user_component_live.ex
+++ lib/phx_chat_web/live/online_user_component_live.ex
@@ -4,7 +4,7 @@ defmodule OnlineUserComponent do
   # TODO ERIC: make stateful, make listen for changes in online users
   def render(assigns) do
     ~L"""
-    <span class="username"><%= @content %></span>
+    <li class="user-list__username"><%= @content %></li>
     """
   end
 end
