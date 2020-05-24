02fff252ba02fbc73111250ae0785d0ebfd812d1
Eric (Martin) Mancini <coelomate@gmail.com>
2020-05-24 10:05:48 -0400

    update styling; ditch monospace vibe

--- priv/repo/migrations/20200503152044_create_messages.exs
+++ priv/repo/migrations/20200503152044_create_messages.exs
@@ -3,6 +3,7 @@ defmodule PhxChat.Repo.Migrations.CreateMessages do
 
   def change do
     create table(:messages) do
+      # TODO ERIC: Ecto defaults this to varchar 255 -- handle long and empty messages
       add :user, :string
       add :message, :string
 
